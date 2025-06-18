6d7491d10ffab9c15b13d5f33c82452d7de3a11b
ddkkmm123 <dasmariapps@gmail.com>
2025-06-18 11:53:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ let bee, mixer, activeAction;
 let animations = [];
 
 const loader = new GLTFLoader();
-loader.load('/demon_bee_full_texture.glb',
+loader.load('demon_bee_full_texture.glb',
     function (gltf) {
         bee = gltf.scene;
         scene.add(bee);
